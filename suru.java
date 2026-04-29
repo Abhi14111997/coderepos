6bd06c9f61740be4e15964c5d5094fd3128b313d
Abhishek Chaubey <119572474+Abhi14111997@users.noreply.github.com>
2026-04-29 11:57:13 +0530

    Update suru.java

---
+++ suru.java
@@ -0,0 +1,13 @@
+public class SimpleMath {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 25;
+        int sum = x + y;
+
+        if (sum > 30) {
+            System.out.println("The sum is: " + sum);
+        } else {
+            System.out.println("Sum is 30 or less.");
+        }
+    }
+}
